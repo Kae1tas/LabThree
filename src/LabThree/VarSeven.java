886c9331af254f647f2b184ec2800201802402ca
Mihail <79074705+Kae1tas@users.noreply.github.com>
2021-08-15 19:14:52 +0900

    1

--- src/LabThree/VarSeven.java
+++ src/LabThree/VarSeven.java
@@ -32,19 +32,80 @@ public class VarSeven {
 
     public static void findIndexMaxAndValue (Node head, int n) {
         Node ref = head;
+
         int indexMax = 0;
-        int valueMax = head.value;
+        int indexMin = 0;
+        int valueMax = 0; //head.value
+        int valueMin = 2147483647;
         int index = 0;
+
         while(ref != null) {
             index++;
-            if (ref.value > valueMax) {
+            if (ref.value > valueMax && (ref.value % 3 == 0)) {
                 valueMax = ref.value;
                 indexMax = index;
             }
-            System.out.println(valueMax);
+            if (ref.value < valueMin && (ref.value % 3 == 0)) {
+                valueMin = ref.value;
+                indexMin = index;
+            }
+            //System.out.println(valueMax);
             ref = ref.next;
         }
-        System.out.println("Номер:" + indexMax + "\nЧисло:" + valueMax);
+
+
+        Node newNodeMax = new Node(valueMax, null);
+        Node newNodeMin = new Node(valueMin, null);
+        ref = head;
+        index = 1;
+        while (ref.next != null && (index < indexMin - 1)) {
+            ref = ref.next;
+            index++;
+        }
+
+        if(indexMin == 1){      //insert instead of first
+            newNodeMax.next = head.next;
+            head = newNodeMax;
+        }
+        else if(indexMin == n){     //insert instead of last
+            ref = head;
+            while (ref.next.next != null) {
+                ref = ref.next;
+            }
+            ref.next = newNodeMax;
+        }
+        else
+            newNodeMax.next = ref.next.next;
+            ref.next = newNodeMax;
+
+
+        ref = head;
+        index = 1;
+
+        while (ref.next != null && (index < indexMax - 1)){
+            ref = ref.next;
+            index++;
+        }
+
+        if (indexMax == 1){
+            newNodeMin.next = head.next;
+            head = newNodeMin;
+        }
+        else if(indexMax == n){
+            ref = head;
+            while (ref.next.next != null){
+                ref = ref.next;
+            }
+            ref.next = newNodeMin;
+        }
+        else{
+            newNodeMin.next = ref.next.next;
+            ref.next = newNodeMin;
+        }
+
+
+        System.out.println("\nНомер наибольшего:" + indexMax + "\nЧисло:" + valueMax + "\n\nНомер наименьшего:" + indexMin + "\nЧисло:" + valueMin);
+        System.out.println(head);
     }
 
     public static void main (String[] args) {
@@ -57,7 +118,5 @@ public class VarSeven {
 
         sumAndAverage(head);
         findIndexMaxAndValue(head, n);
-
-
     }
 }
